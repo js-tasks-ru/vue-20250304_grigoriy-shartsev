450cab6bb262149e8fd783839dc79cdf0a8c6d4d
Julia Gordeeva <gordeeva.ia@parma.ru>
2025-04-14 23:48:07 +0500

    solve 02-basics-2

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,57 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
-  setup() {},
+  setup() {
+    const a = ref(0)
+    const b = ref(0)
+    const operand = ref("sum")
+
+    const result = computed(() => {
+      if (operand.value === "sum") {
+        return a.value + b.value;
+      }
+
+      if (operand.value === "subtract") {
+        return a.value - b.value;
+      }
+
+      if (operand.value === "multiply") {
+        return a.value * b.value;
+      }
+
+      if (operand.value === "divide") {
+        return a.value / b.value;
+      }
+
+      return null;
+    })
+
+    return {
+      a,
+      b,
+      operand,
+      result,
+    }
+  },
 
   template: `
     <div class="calculator">
-      <input type="number" aria-label="First operand" />
+      <input type="number" aria-label="First operand" v-model="a"/>
 
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" value="divide"/>➗</label>
+        <label><input type="radio" name="operator" value="sum" v-model="operand"/>➕</label>
+        <label><input type="radio" name="operator" value="subtract" v-model="operand"/>➖</label>
+        <label><input type="radio" name="operator" value="multiply" v-model="operand"/>✖</label>
+        <label><input type="radio" name="operator" value="divide" v-model="operand"/>➗</label>
       </div>
 
-      <input type="number" aria-label="Second operand" />
+      <input type="number" aria-label="Second operand" v-model="b"/>
 
       <div>=</div>
 
-      <output>0</output>
+      <output>{{ result }}</output>
     </div>
   `,
 })
